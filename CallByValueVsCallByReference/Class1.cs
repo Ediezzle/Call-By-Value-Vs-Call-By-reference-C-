62c9ee5a5e01998a520dd9de273bc1760d0423b0
Ediezzle <edmore.mandikiyana@gmail.com>
2019-12-29 18:31:12 +0200

    once off commit

---
+++ CallByValueVsCallByReference/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CallByValueVsCallByReference
+{
+      class Class1
+    {
+        public static int add(params int[] array)
+        {
+            int sum = 0;
+            foreach(int value in array)
+            {
+                sum += value;
+            }
+            return sum;
+        }
+
+       }
+}
---
+++ CallByValueVsCallByReference/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CallByValueVsCallByReference
+{
+    class Program
+    {
+ 
+             public void ChangeSize(int size)
+            {
+
+                size *= 5;
+            }
+
+            public static void ChangeSize(ref int size)
+            {
+                size*= 5;
+
+            }
+
+            static void Main(string[] args)
+            {
+                Program p = new Program();
+                int size = 12;
+                Console.WriteLine("Main's size is {0}", size);
+                p.ChangeSize(size);
+                Console.WriteLine("Method's size is " + size);
+                Console.WriteLine("Main's size is now " + size);
+
+                Console.WriteLine();
+
+                Console.WriteLine("Main's size is " + size);
+                ChangeSize(ref size);
+                Console.WriteLine("Method's size is " + size);
+                Console.WriteLine("Main's size is now " + size);
+
+            int[] array = { 1, 2, 3 };
+            Console.WriteLine(Class1.add(array)); 
+            }
+        }
+    }
+
+
